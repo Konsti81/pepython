5c37cad29163cd266ab3723c85e4193b4fccde43
Bbalazzs <nemdivatszurkolo@gmail.com>
2024-11-28 20:15:07 +0100

    program kezdésének feltöltése.

---
+++ jelszo.py
@@ -0,0 +1,20 @@
+import random
+import string
+felhasznalonev=input("Adj meg egy felhasználó nevet!: ")
+jelszo=0
+print("1. Új felhasználónév-jelszó pár létrehozás")
+print("2. Meglévő felhasznűló-jelszó pár módositása")
+print("3. Meglévő felhasználónév-jelszó pár törlése")
+menüpont_szama=int(input("Kérem a kiválasztott menüpont számát:"))
+if menüpont_szama==1:
+    #print("")
+    #szavak imprtálása txt fáljból.
+    #szavak=[2 random szó a szótárból]szavak.append()random.choices(szotar,2)
+    
+    #szam=random.randit(100,999)
+    #speckaraterter=random.choice(string.puntuation)
+if menüpont_szama==2:
+    print("")
+if menüpont_szama==3:
+    print("")
+
