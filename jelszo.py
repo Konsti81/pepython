122b6c6d8420fd6c975ed0e5e755d58906acf4a7
Konsti81 <konstantin.olah2@gmail.com>
2024-12-01 17:12:49 +0100

    Szótár hozzáadása

--- jelszo.py
+++ jelszo.py
@@ -1,20 +1,26 @@
 import random
 import string
+szotar=["alma", "kereszt", "fal","kő", "kapu", "elem", "lista", "ragasztó"]
 felhasznalonev=input("Adj meg egy felhasználó nevet!: ")
-jelszo=0
+def jelszogeneralas():
+    szavak=random.choices(szotar,2)
+    szam=random.randit(100,999)
+    speckaraterter=random.choice(string.puntuation)
+    return f"{szavak}{szam}{speckaraterter}"
+jelszo=jelszogeneralas()
 print("1. Új felhasználónév-jelszó pár létrehozás")
 print("2. Meglévő felhasznűló-jelszó pár módositása")
 print("3. Meglévő felhasználónév-jelszó pár törlése")
-menüpont_szama=int(input("Kérem a kiválasztott menüpont számát:"))
-if menüpont_szama==1:
-    #print("")
+menupont_szama=int(input("Kérem a kiválasztott menüpont számát:"))
+if menupont_szama==1:
+    print(f"A felhasználóneved:{felhasznalonev} És az ehhez tartozó jelszavad:{jelszo}")
     #szavak imprtálása txt fáljból.
     #szavak=[2 random szó a szótárból]szavak.append()random.choices(szotar,2)
     
     #szam=random.randit(100,999)
     #speckaraterter=random.choice(string.puntuation)
-if menüpont_szama==2:
+if menupont_szama==2:
     print("")
-if menüpont_szama==3:
+if menupont_szama==3:
     print("")
 
