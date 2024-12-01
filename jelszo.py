ca1219a501ac0744946b597180b3f4ea70eedb5a
martinuzzy <156809397+martinuzzy@users.noreply.github.com>
2024-12-01 18:28:35 +0100

    almenü pontok hozzáadása a "Meglévő felhasználónév-jelszó pár módosítása" menüponthoz

--- jelszo.py
+++ jelszo.py
@@ -9,7 +9,7 @@ def jelszogeneralas():
     return f"{szavak[0]}{szavak[1]}{szam}{speckaraterter}"
 jelszo=jelszogeneralas()
 print("1. Új felhasználónév-jelszó pár létrehozás")
-print("2. Meglévő felhasznűló-jelszó pár módositása")
+print("2. Meglévő felhasznűlónév-jelszó pár módositása")
 print("3. Meglévő felhasználónév-jelszó pár törlése")
 menupont_szama=int(input("Kérem a kiválasztott menüpont számát:"))
 if menupont_szama==1:
@@ -20,7 +20,16 @@ if menupont_szama==1:
     #szam=random.randit(100,999)
     #speckaraterter=random.choice(string.puntuation)
 if menupont_szama==2:
-    print("")
+    print("1. Meglévő felhasználónév-jelszó pár újragenerálása")
+    print("2. Meglévő felhasználónév módosítása")
+    print("Meglévő jelszó módosítása")
+    almenu2_szama=int(input(print("Kérem a kiválasztott menüpont számát:")))
+    if almenu2_szama==1:
+        print("")
+    if almenu2_szama==2:
+        print("")
+    if almenu2_szama==3:
+        print("")
 if menupont_szama==3:
     print("")
 
